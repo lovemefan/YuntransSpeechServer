4801b65fbf3b23b7bb2f22120692a004b1c3b24e
lovemefan <450489712@qq.com>
2021-08-10 09:13:44 +0800

    Initial commit

---
+++ websocketServer/src/test/java/com/yuntrans/websocketserver/WebsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuntrans.websocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
