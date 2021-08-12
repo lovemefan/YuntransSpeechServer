e305cf781cc76bd10b202f807017a74c7d306a90
lovemefan <450489712@qq.com>
2021-08-12 17:33:53 +0800

    init websocket config

--- websocketServer/src/main/java/com/yuntrans/websocketserver/config/WebsocketConfig.java
+++ websocketServer/src/main/java/com/yuntrans/websocketserver/config/WebsocketConfig.java
@@ -1,14 +1,33 @@
 package com.yuntrans.websocketserver.config;
 
+import com.yuntrans.websocketserver.handle.WsHandle;
+import com.yuntrans.websocketserver.interceptor.WsHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+import javax.annotation.Resource;
+
 @Configuration
-public class WebsocketConfig {
-    @Bean
-    // 自动注入ServerEndpointer bean对象，自动注册使用了@ServerEndpoint的bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
+@EnableWebSocket
+public class WebsocketConfig  implements WebSocketConfigurer {
+
+    @Autowired
+    private WsHandshakeInterceptor wsHandshakeInterceptor;
+
+    @Autowired
+    private WsHandle wsHandle;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry
+                .addHandler(wsHandle, "/v1/api/asr")
+                .addInterceptors(wsHandshakeInterceptor)
+                .setAllowedOrigins("*");
     }
 }
