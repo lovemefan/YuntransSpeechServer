c1ff93b7247f1aaf21fb73d3c1350b2f0b008b72
lovemefan <450489712@qq.com>
2021-08-12 17:31:49 +0800

    init status enum

---
+++ websocketServer/src/main/java/com/yuntrans/websocketserver/wsEnum/WsStatus.java
@@ -0,0 +1,34 @@
+/**
+ * @Time : 2021/8/12 16:09
+ * @Author : lovemefan
+ * @Email : lovemefan@outlook.com
+ */
+package com.yuntrans.websocketserver.wsEnum;
+
+public enum WsStatus {
+    SUCCESS(10000),
+    FAILED(10001),
+    UNAUTHORIZAED(10002),
+    FORBIDDEN(10003),
+    TIMEOUT(10004),
+    ;
+
+
+    private Integer statusCode;
+    private WsStatus(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String toString() {
+        return statusCode.toString();
+    }
+}
