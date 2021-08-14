c51f174a5b9f9e84bd22c2223c3212f28815e2ec
lovemefan <450489712@qq.com>
2021-08-14 18:10:07 +0800

    add redis config

---
+++ websocketServer/src/main/java/com/yuntrans/websocketserver/config/RedisConfig.java
@@ -0,0 +1,39 @@
+/**
+ * @Time : 2021/8/14 18:06
+ * @Author : lovemefan
+ * @Email : lovemefan@outlook.com
+ */
+package com.yuntrans.websocketserver.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+/**
+ * cache 配置类
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+}
