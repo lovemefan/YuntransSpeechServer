f4d54c9b2089a0bb065f166bc2ac50a30c3b1987
lovemefan <450489712@qq.com>
2021-08-15 17:28:27 +0800

    init rocketmq module

---
+++ rocketmq/src/test/java/com/example/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
