4f21225341614d0cd909b1e4165eb5d2b13538da
lovemefan <450489712@qq.com>
2021-08-15 17:27:39 +0800

    init gateway module

---
+++ gateway/src/test/java/com/yuntrans/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuntrans.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
