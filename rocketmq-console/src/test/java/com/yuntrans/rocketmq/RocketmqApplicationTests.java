5d915954cf641051881ba1a377de29f198226747
lovemefan <450489712@qq.com>
2021-08-16 14:14:14 +0800

    add rocketmq console

---
+++ rocketmq-console/src/test/java/com/yuntrans/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuntrans.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
