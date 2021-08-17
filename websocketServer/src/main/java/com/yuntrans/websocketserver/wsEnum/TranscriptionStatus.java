ecc002164e2866277f16d2b6a3b325b9438b9127
lovemefan <450489712@qq.com>
2021-08-17 15:27:29 +0800

    init TranscriptionStatus

---
+++ websocketServer/src/main/java/com/yuntrans/websocketserver/wsEnum/TranscriptionStatus.java
@@ -0,0 +1,12 @@
+package com.yuntrans.websocketserver.wsEnum;
+
+/**
+ * @Time : 2021/8/16 20:45
+ * @Author : lovemefan
+ * @Email : lovemefan@outlook.com
+ */
+public enum TranscriptionStatus {
+    START,
+    PARTIAL,
+    FINAL;
+}
