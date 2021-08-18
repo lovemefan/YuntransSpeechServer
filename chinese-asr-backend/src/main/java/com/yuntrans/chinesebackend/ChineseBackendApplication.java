6885804701535e8e09f103f1ae1d881ec1c9f248
lovemefan <450489712@qq.com>
2021-08-18 15:33:29 +0800

    init chinese asr backend module

---
+++ chinese-asr-backend/src/main/java/com/yuntrans/chinesebackend/ChineseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yuntrans.chinesebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChineseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChineseBackendApplication.class, args);
+    }
+
+}
