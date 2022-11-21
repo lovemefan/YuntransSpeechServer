f80ab5888c0a8486c4423d302acd4682ff4610bc
lovemefan <lovemefan@outlook.com>
2022-11-21 22:15:24 +0800

    add auth status  interface for all status such as YuntransStatus, GatewayStatus

--- common/src/main/java/com/yuntrans/common/status/YuntransStatus.java
+++ common/src/main/java/com/yuntrans/common/status/YuntransStatus.java
@@ -10,36 +10,36 @@ import com.yuntrans.common.exception.RetryableException;
 import com.yuntrans.common.exception.YuntransException;
 import com.yuntrans.common.utils.Json;
 
-public class YuntransStatus {
-    public static final YuntransStatus SUCCESS = new YuntransStatus(20000000, "SUCCESS", "Success.");
+public enum YuntransStatus implements Status {
+    SUCCESS(20000000, "SUCCESS", "Success."),
 
-    public static final YuntransStatus COMMON_ERROR = new YuntransStatus(30000000, "COMMON_ERROR", "Common error!");
+    COMMON_ERROR(30000000, "COMMON_ERROR", "Common error!"),
 
-    public static final YuntransStatus CLIENT_ERROR = new YuntransStatus(40000000, "CLIENT_ERROR", "Client error!");
+    CLIENT_ERROR(40000000, "CLIENT_ERROR", "Client error!"),
 
-    public static final YuntransStatus ACCESS_DENIED = new YuntransStatus(40000001, "ACCESS_DENIED", "Access denied!");
+    ACCESS_DENIED(40000001, "ACCESS_DENIED", "Access denied!"),
 
-    public static final YuntransStatus MESSAGE_INVALID = new YuntransStatus(40000002, "MESSAGE_INVALID", "Message invalid!");
+    MESSAGE_INVALID(40000002, "MESSAGE_INVALID", "Message invalid!"),
 
-    public static final YuntransStatus PARAMETER_INVALID = new YuntransStatus(40000003, "PARAMETER_INVALID", "Parameter invalid!");
+    PARAMETER_INVALID(40000003, "PARAMETER_INVALID", "Parameter invalid!"),
 
-    public static final YuntransStatus IDLE_TIMEOUT = new YuntransStatus(40000004, "IDLE_TIMEOUT", "Idle timeout!");
+    IDLE_TIMEOUT(40000004, "IDLE_TIMEOUT", "Idle timeout!"),
 
-    public static final YuntransStatus TOO_MANY_REQUESTS = new YuntransStatus(40000005, "TOO_MANY_REQUESTS", "Too many requests!");
+    TOO_MANY_REQUESTS(40000005, "TOO_MANY_REQUESTS", "Too many requests!"),
 
-    public static final YuntransStatus PAGE_NUMBER_INVALID = new YuntransStatus(40000006, "PAGE_NUMBER_INVALID", "Page number is invalid!");
+    PAGE_NUMBER_INVALID(40000006, "PAGE_NUMBER_INVALID", "Page number is invalid!"),
 
-    public static final YuntransStatus PAGE_SIZE_INVALID = new YuntransStatus(40000007, "PAGE_SIZE_INVALID", "Page size is invalid!");
+    PAGE_SIZE_INVALID(40000007, "PAGE_SIZE_INVALID", "Page size is invalid!"),
 
-    public static final YuntransStatus PAGE_OFFSET_TOO_LARGE = new YuntransStatus(40000008, "PAGE_OFFSET_TOO_LARGE", "Page size is invalid!");
+    PAGE_OFFSET_TOO_LARGE(40000008, "PAGE_OFFSET_TOO_LARGE", "Page size is invalid!"),
 
-    public static final YuntransStatus AUDIO_DECODE_ERROR = new YuntransStatus(40000009, "AUDIO_DECODE_ERROR", "Decode audio error!");
+    AUDIO_DECODE_ERROR(40000009, "AUDIO_DECODE_ERROR", "Decode audio error!"),
 
-    public static final YuntransStatus FREE_TRIAL_EXPIRED = new YuntransStatus(40000010, "FREE_TRIAL_EXPIRED", "The free trial has expired!");
+    FREE_TRIAL_EXPIRED(40000010, "FREE_TRIAL_EXPIRED", "The free trial has expired!"),
 
-    public static final YuntransStatus SERVER_ERROR = new YuntransStatus(50000000, "SERVER_ERROR", "Server error!");
+    SERVER_ERROR(50000000, "SERVER_ERROR", "Server error!"),
 
-    public static final YuntransStatus GRPC_ERROR = new YuntransStatus(50000001, "GRPC_ERROR", "Grpc error!");
+    GRPC_ERROR(50000001, "GRPC_ERROR", "Grpc error!");
 
     public static final String DELIMITER = ":";
 
@@ -47,18 +47,18 @@ public class YuntransStatus {
 
     private final String name;
 
-    private final String text;
+    private String text;
 
-    private final Throwable cause;
+    private Throwable cause;
 
-    public YuntransStatus(int code, String name, String text) {
+    YuntransStatus(int code, String name, String text) {
         this.code = code;
         this.name = name;
         this.text = text;
         this.cause = null;
     }
 
-    private YuntransStatus(int code, String name, String text, Throwable cause) {
+    YuntransStatus(int code, String name, String text, Throwable cause) {
         this.code = code;
         this.name = name;
         this.text = text;
@@ -66,15 +66,19 @@ public class YuntransStatus {
     }
 
     public YuntransStatus modify(String text) {
-        return new YuntransStatus(this.code, this.name, text, this.cause);
+        this.text=text;
+        return this;
     }
 
     public YuntransStatus modify(String text, Throwable cause) {
-        return new YuntransStatus(this.code, this.name, text, cause);
+        this.text = text;
+        this.cause = cause;
+        return this;
     }
 
     public YuntransStatus modify(Throwable cause) {
-        return new YuntransStatus(this.code, this.name, this.text, cause);
+        this.cause = cause;
+        return this;
     }
 
     public YuntransException toException() {
